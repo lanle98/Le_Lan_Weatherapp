f05fbd2cd17ef1c78d7beaa485f78045dda69a65
Darren Le <darrenle98@gmail.com>
2019-12-27 23:08:22 -0500

    added ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,37 @@
   console.log("fired");
 
   //declare variables
-  let dataMain = document.querySelector(".main");
+  let dataMain = document.querySelector(".main"),
+    submit = document.querySelector("button");
+
+  let temp = document.querySelector(".temp");
+
+  function parseData(data) {
+    temp.innerHTML = data.main.temp;
+  }
+
+  function fetchData(e) {
+    e.preventDefault();
+    console.log(e);
+    let link = "https://api.openweathermap.org/data/2.5/weather?q=";
+    let api_key = "&appid=05bb610c5b145f70cd91f5ad63c99529";
+    let celcius = "&units=metric";
+    let input = document.querySelector("input");
+    let city = input.value;
+    let url = link + city + api_key + celcius;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        if (data.main == undefined) {
+          temp.innerHTML = "We cannot find this city, try again";
+        } else {
+          parseData(data);
+        }
+      });
+  }
+
+  submit.addEventListener("click", fetchData);
 
   function settingUI(main) {
     //setting UI for weather
